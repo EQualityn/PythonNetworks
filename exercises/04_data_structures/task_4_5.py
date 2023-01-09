52a9a1b4c799e86fe2c73659c04e6ab1ec2140c1
EQualityn <egor120202@gmail.com>
2023-01-09 11:55:28 +0200

    Task 4_5 complete

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,8 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+result = []
+for trunk in (command1.split()[-1]).split(','):
+    if trunk in (command1.split()[-1]).split(',') and trunk in (command2.split()[-1]).split(','):
+        result.append(trunk)
+print(result)
